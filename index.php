b154e9d3ec3660fdc02e4db0bb65a95073eef15b
Surya Wiguna <suryawigunaa@gmail.com>
2019-10-23 11:41:28 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include "layout/navbar.php" ?>
+    <div class="content">
+        <div class="jumbotron px-0 pb-0 bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 order-2 mt-4 mt-lg-0">
+                        <h1 class="display-4 font-weight-bold mb-4">MBX is one stop solution for your needs</h1>
+                        <a href="#" class="btn btn-primary mt-2"><h5 class="m-0 p-2">Learn More</h5></a>
+                    </div>
+                    <div class="col-lg-7 order-1 order-lg-2">
+                        <img src="img/undraw_best_place_r685.png" alt="" width="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row section-div">
+                <div class="col-lg-5">
+                    <img src="img/chart.jpeg" class="rounded" alt="" width="100%">
+                </div>
+                <div class="col-lg-7 px-auto px-lg-5 d-flex flex-column align-self-center">
+                    <h1 class="display-5 mt-4 mt-lg-0 font-weight-bold">We have what you're looking for</h1>
+                    <p>From social media, mall, to online transport, and ticketing. Solve your problems with our services.</p>
+                </div>
+            </div>
+            <div class="row section-div">
+                <div class="col-lg-4">
+                    <div class="card p-2 my-2 my-lg-0 border-0 rounded-lg bg-danger">
+                        <div class="card-body text-white">
+                            <h3 class="card-title font-weight-bold">Visi MBX</h3>
+                            <p class="card-text">Menjadi kehidupan baru bagi penduduk dunia tahun 2025.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card p-2 my-2 my-lg-0 border-0 rounded-lg bg-warning">
+                        <div class="card-body text-white">
+                            <h3 class="card-title font-weight-bold">Misi MBX</h3>
+                            <ol>
+                                <li class="card-text">Membangun komunitas bisnis to bisnis dengan jaringan yang kuat mendunia dan menguntungkan</li>
+                                <li class="card-text">Mengembangkan seluruh program-program dalam MBX sehingga menjadi aplikasi online yang paling dicari di dunia</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card p-2 my-2 my-lg-0 border-0 rounded-lg bg-primary">
+                        <div class="card-body text-white">
+                            <h3 class="card-title font-weight-bold">Program MBX</h3>
+                            <ol>
+                                <li class="card-text">Membangun komunitas bisnis yang kuat dalam program networking</li>
+                                <li class="card-text">Menggandeng merchant dari berbagai lini bisnis dan UMKM di dunia</li>
+                                <li class="card-text">Menyediakan layanan jasa dan produk berkualitas yang menjadi kebutuhan pokok masyarakat, sehingga menguatkan kesejahteraan ekonomi dalam komunitas MBX</li>
+                                <li class="card-text">Mengedukasi masyarakat tentang digital entrepreneurship dalam komunitas MBX</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row section-div justify-content-center">
+                <h1 class="text-center font-weight-bold mb-4">How To Register?</h1>
+                <img src="img/WhatsApp Image 2018-11-27 at 16.15.28.jpeg" alt="" width="100%">
+            </div>
+        </div>
+    </div>
+<?php include "layout/footer.php" ?>
---
+++ layout/navbar.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo1.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                <span class="font-weight-bold">MBX</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item my-lg-0 my-2 active">
+                        <a class="nav-link text-center" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2">
+                        <a class="nav-link text-center" href="#">Buy PIN</a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2">
+                        <a class="nav-link text-center" href="#">Login Member</a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2 featured-link bg-primary">
+                        <a class="nav-link text-center text-white" href="#">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
