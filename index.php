172672f10ca1b9d76320fb1686abf296de0ccf27
Surya Wiguna <suryawigunaa@gmail.com>
2019-10-24 11:01:28 +0800

    login and register page

--- index.php
+++ index.php
@@ -1,14 +1,56 @@
-<?php include "layout/navbar.php" ?>
-    <div class="content">
-        <div class="jumbotron px-0 pb-0 bg-white">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="img/favicon.ico">
+    <title>MBX</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.materialdesignicons.com/4.5.95/css/materialdesignicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top py-3">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo1.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                <span class="font-weight-bold">MBX</span>
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="mdi mdi-menu mdi-24px text-white"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item my-lg-0 my-2 active">
+                        <a class="nav-link text-center text-white" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2">
+                        <a class="nav-link text-center text-white" href="#">Buy PIN</a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2">
+                        <a class="nav-link text-center text-white" href="login.php">Login Member</a>
+                    </li>
+                    <li class="nav-item my-lg-0 my-2 featured-link bg-warning">
+                        <a class="nav-link text-center text-white" href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="content p-0">
+        <div class="jumbotron bg-primary text-white">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-5 order-2 mt-4 mt-lg-0">
+                    <div class="col-lg-5 order-2 my-5">
                         <h1 class="display-4 font-weight-bold mb-4">MBX is one stop solution for your needs</h1>
-                        <a href="#" class="btn btn-primary mt-2"><h5 class="m-0 p-2">Learn More</h5></a>
                     </div>
-                    <div class="col-lg-7 order-1 order-lg-2">
-                        <img src="img/undraw_best_place_r685.png" alt="" width="100%">
+                    <div class="col-lg-7 order-1 order-lg-2 mt-3">
+                        <img src="img/undraw_best_place_r685.svg" alt="" width="100%">
                     </div>
                 </div>
             </div>
@@ -58,9 +100,65 @@
                 </div>
             </div>
             <div class="row section-div justify-content-center">
-                <h1 class="text-center font-weight-bold mb-4">How To Register?</h1>
-                <img src="img/WhatsApp Image 2018-11-27 at 16.15.28.jpeg" alt="" width="100%">
+                <div class="col-12">
+                    <h1 class="text-center font-weight-bold mb-4">How To Register and Buy PIN?</h1>
+                </div>
+                <div class="col-lg-6 my-2 my-lg-0">
+                    <img src="img/WhatsApp Image 2018-11-27 at 16.15.28.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-6 my-2 my-lg-0">
+                    <img src="img/WhatsApp Image 2018-11-27 at 16.14.24.jpeg" alt="" width="100%">
+                </div>
+            </div>
+            <div class="row section-div justify-content-center">
+                <div class="col-12">
+                    <h1 class="text-center font-weight-bold mb-4">Our Products and Services</h1>
+                </div>
+                <div class="col-lg-4 my-2">
+                    <img src="img/my-box-social-media.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-4 my-2">
+                    <img src="img/mbx-mall.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-4 my-2">
+                    <img src="img/mbx-mall2.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-4 my-2">
+                    <img src="img/promo-50-pin-get-10-pin.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-4 my-2">
+                    <img src="img/mbx-mall3.jpeg" alt="" width="100%">
+                </div>
+                <div class="col-lg-4 my-2">
+                </div>
             </div>
         </div>
+        <div class="section-div justify-content-center bg-primary text-white">
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-lg-6 mb-3">
+                        <h4 class="font-weight-bold text-center">Customer Service</h4>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="d-flex flex-row">
+                            <span class="mdi mdi-map-marker mdi-24px mr-2"></span>
+                            <p class="m-0 align-self-center">Jl. Tukad Baru Pulau Batanta No. 2B, Denpasar</p>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <span class="mdi mdi-email mdi-24px mr-2"></span>
+                            <p class="m-0 align-self-center">cs@mbx.co.id</p>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <span class="mdi mdi-whatsapp mdi-24px mr-2"></span>
+                            <p class="m-0 align-self-center">082 147 118 069</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="copyright" class="d-flex text-white justify-content-center py-4" style="background-color: #006de3">
+            <small class="m-0 text-center">Copyright © Mybox. All rights reserved PT. MITRA BISNIS EKSEKUTIF</small>
+        </div>
     </div>
-<?php include "layout/footer.php" ?>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="img/favicon.ico">
+    <title>Login - MBX</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.materialdesignicons.com/4.5.95/css/materialdesignicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body class="bg-primary">
+    <div class="d-flex justify-content-center align-items-center" style="height:100vh;">
+        <div class="col-lg-4 col-md-6 col-sm-8">
+            <div class="d-flex flex-row justify-content-center align-items-center mb-4">
+                <img src="img/logo1.png" alt="logo" height="60px">
+                <h3 class="font-weight-bold text-white m-0">MyBoX</h3>
+            </div>
+            <div class="card card-radius border-0 p-3">
+                <div class="card-header border-0 bg-white">
+                    <h5 class="text-center mt-2 mb-2 font-weight-bold">Login Member</h5>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="" aria-label="">
+                        <div id="form-div" class="d-flex flex-column p-0 mb-4">
+                            <small>Email</small>
+                            <input type="email" class="form-control bg-light" name="email" placeholder="email@example.com" required autofocus>
+                        </div>
+                        <div id="form-div" class="d-flex flex-column p-0 mb-4">
+                            <small>Password</small>
+                            <input type="password" class="form-control bg-light" name="password" placeholder="............." required>
+                        </div>
+                        <button type="submit" id="btn-login" class="btn btn-block btn-lg bg-primary text-white font-weight-bold m-0"><i class="mdi mdi-login-variant mr-1"></i>Login</button>
+                    </form>
+                    <a href="register.php" class="btn btn-sm btn-block btn-outline-warning font-weight-bold mt-2">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
