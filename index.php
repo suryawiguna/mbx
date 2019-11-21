e7ed229d2ea842f85cb5bad570000becb4d778b3
Surya Wiguna <suryawigunaa@gmail.com>
2019-11-21 22:50:25 +0800

    spinning logo?

--- index.php
+++ index.php
@@ -13,6 +13,23 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style>
+        #rotate {
+            animation-name: spin;
+            animation-duration: 5000ms;
+            animation-iteration-count: infinite;
+            animation-timing-function: linear; 
+        }
+        
+        @keyframes spin {
+            from {
+            transform: rotate(0deg);
+            }
+            to {
+            transform: rotate(359deg);
+            }
+        }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top py-3">
@@ -71,6 +88,11 @@
                     <a href="#" class="pop">
                         <img src="img/new/2.jpg" class="d-block w-100">
                     </a>
+                </div>
+            </div>
+            <div class="row section-div">
+                <div class="col-lg-6 offset-lg-3 my-4">
+                    <img src="img/mbx.png" class="d-block w-100" id="rotate">
                 </div>
             </div>
             <div class="row section-div">
